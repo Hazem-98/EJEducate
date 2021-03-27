e62946c5dec5bac7944d829025e17a250b1e26b0
Hazem-98 <63345059+Hazem-98@users.noreply.github.com>
2021-03-27 13:19:09 +0100

    Add files via upload

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyA_rVdz8jankSZdAc7Tx594KGCtUuLeSIg",
+    authDomain: "ejeducate.firebaseapp.com",
+    projectId: "ejeducate",
+    storageBucket: "ejeducate.appspot.com",
+    messagingSenderId: "220052154640",
+    appId: "1:220052154640:web:ea32952fda3894d672dd95",
+    measurementId: "G-396Q2EPRJ5",
+
+}
